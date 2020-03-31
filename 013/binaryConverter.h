3b774a737e5a64e24a944a6f3268cfe9849d4dfd
IBOTIAndy_PC_NTp <visua64@gmail.com>
2020-03-31 14:15:06 +0800

    數位電路模擬完成

---
+++ 013/binaryConverter.h
@@ -0,0 +1,8 @@
+#ifndef BINARY_CONVERTER_H
+#define BINARY_CONVERTER_H
+
+extern int power(int x, int p);
+extern int to10(int x, int b);
+extern int toN(int x, int b);
+
+#endif // BINARY_CONVERTER_C
