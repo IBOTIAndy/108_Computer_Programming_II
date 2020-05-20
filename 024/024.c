50c94915bbd0df3afb0a3a4728617f50a6a3f90e
IBOTIAndy <36949276+IBOTIAndy@users.noreply.github.com>
2020-05-20 21:32:03 +0800

    cutString is completed.

--- 024/024.c
+++ 024/024.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 //024 字串修改
-//2020/05/20 PM. 09:10 IBOTIAndy
+//2020/05/20 PM. 09:31 IBOTIAndy
 void input(char *string, char *wordP, char *wordQ){
     gets(string);
     gets(wordP);
     gets(wordQ);
+}
+
+char* strcut(char *string, char cut){
+    while(*string != '\0'){
+        if(*string == cut){
+            *string = '\0';
+            return string+1;
+        }
+        string++;
+    }
+    return NULL;
+}
+
+void cutString(char *string, char *pString[]){
+    int i=0;
+    while(string != NULL){ //直到全部切割完
+        pString[i] = string;
+        string = strcut(string, ' ');
+        i = i + 1;
+    }
+    pString[i] = NULL;
 }
 
 void f1(){
@@ -15,8 +36,7 @@ void f1(){
     char *pString[500];
     input(string, wordP, wordQ);
 //    printf("%s\n%s\n%s\n", string, wordP, wordQ);
-
-    AreplaceB(string);
+    cutString(string, pString);
 }
 
 int main(){
